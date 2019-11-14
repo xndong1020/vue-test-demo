2a8c258fbc4090fb6acb48831141e88719ac1c2f
JGU <jgu@networkten.com.au>
2019-11-15 01:56:10 +1100

    add a global  plugin

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import GlobalProperties from './plugins/GlobalProperties'
+
 Vue.config.productionTip = false
+Vue.use(GlobalProperties)
 
 new Vue({
   router,
