960e8c197cb4774a6b9cd1af5617d8dbdb4a6d08
Jeremy Gu <jgu@networkten.com.au>
2019-11-14 16:23:53 +1100

    error babel

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 packages we need for doing unit testing
 
 ```
-npm i -D jest @vue/test-utils vue-jest babel-jest node-sass sass-loader jest-serializer-vue flush-promises jest-transform-stub babel-core@7.0.0-bridge.0 @babel/core @babel/preset-env @types/jest
+npm i -D jest @vue/test-utils vue-jest babel-jest node-sass sass-loader jest-serializer-vue flush-promises jest-transform-stub babel-core@7.0.0-bridge.0 @babel/core @babel/preset-env @types/jest babel-plugin-transform-runtime babel-preset-env babel-preset-stage-2
 ```
 
 @vue/test-utils makes mounting new components lot easier, and have some helper methods
--- src/components/blog/__tests__/BlogPosts.spec.js
+++ src/components/blog/__tests__/BlogPosts.spec.js
@@ -2,15 +2,39 @@ import Vuex from 'vuex'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import BlogPosts from '@/components/blog/BlogPosts'
 import Loader from '@/components/Loader'
+import flushPromise from 'flush-promises'
+import { getters, mutations, actions } from '@/store/modules/blog'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
+
+jest.mock('@/api', () => ({
+  getBlogPosts: jest.fn(() => {
+    return new Promise(resolve => {
+      global.process.nextTick(() => {
+        resolve({ data: [{ title: 'title 1' }, { title: 'title 2' }] })
+      })
+    })
+  })
+}))
 
 describe('Testing BlogPosts.vue', () => {
   let wrapper
   let store
   beforeEach(() => {
-    store = new Vuex.Store({})
+    store = new Vuex.Store({
+      modules: {
+        blog: {
+          namespaced: true,
+          state: {
+            blogPosts: []
+          },
+          getters,
+          mutations,
+          actions
+        }
+      }
+    })
     wrapper = shallowMount(BlogPosts, {
       localVue,
       store,
@@ -24,7 +48,9 @@ describe('Testing BlogPosts.vue', () => {
     expect(wrapper.contains('div.blog-posts')).toBe(true)
   })
 
-  it('shows loader initially, and hides when posts have loaded', () => {
+  it('shows loader initially, and hides when posts have loaded', async () => {
     expect(wrapper.contains('.loader')).toBe(true)
+    await flushPromise()
+    expect(wrapper.contains('.loader')).toBe(false)
   })
 })
