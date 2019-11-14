52e5701d8fd8f2b6724d871579b5fe06f4c6a1d2
Jeremy Gu <jgu@networkten.com.au>
2019-11-14 15:40:59 +1100

    fixed store

--- src/components/blog/BlogPosts.vue
+++ src/components/blog/BlogPosts.vue
@@ -6,12 +6,20 @@
 </template>
 
 <script>
-import Loader from '@/components/Loader'
+import { mapGetters } from "vuex";
+import Loader from "@/components/Loader";
 export default {
   components: {
     Loader
+  },
+  computed: {
+    ...mapGetters("blog", ["blogPosts", "hasBlogPosts", "numberOfPosts"])
+  },
+  created() {
+    if (!this.hasBlogPosts) this.$store.dispatch("blog/getBlogPosts");
   }
-}
+};
 </script>
 
 <style scoped></style>
+
---
+++ src/store/modules/blog.js
@@ -0,0 +1,35 @@
+import { getBlogPosts, getComments } from '@/api'
+export const state = {
+  blogPosts: []
+}
+
+export const getters = {
+  blogPosts: state => state.blogPosts,
+  hasBlogPosts: state => state.blogPosts.length > 0,
+  numberOfPosts: state => state.blogPosts.length
+}
+
+export const mutations = {
+  saveBlogPosts(state, blogPosts) {
+    state.blogPosts = blogPosts
+  }
+}
+
+export const actions = {
+  getBlogPosts({ commit }) {
+    return getBlogPosts()
+      .then(res => {
+        console.log('action', res.data)
+        commit('saveBlogPosts', res.data)
+      })
+      .catch(error => console.log('error', error))
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
