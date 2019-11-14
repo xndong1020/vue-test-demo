1d006e245de579fcd980099dd84e4b0ba6c2c3a1
JGU <jgu@networkten.com.au>
2019-11-14 23:18:04 +1100

    fix babel config

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,16 @@ module.exports = {
       'jest-transform-stub'
   },
   transform: {
-    '^.+.vue$': 'vue-jest',
-    '^.+.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.(js|jsx)?$': 'babel-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
       'jest-transform-stub'
   },
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch: [
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/lib/**/*.vue', '!<rootDir>/test/**']
-}
+};
